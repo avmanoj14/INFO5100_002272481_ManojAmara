80a64fbfbeb50a7649afc898229229ff76975e21
avmanoj14 <amara.manoj14@gmail.com>
2023-10-11 13:29:37 -0700

    made changes in bottle class

--- Exercises/exercise0/src/Bottle.java
+++ Exercises/exercise0/src/Bottle.java
@@ -22,17 +22,6 @@ public class Bottle {
         System.out.println("Bottle instance created. Bottle ID: " + this.bottleId);
     }
 
-    public void displayInfo() {
-        System.out.println("Bottle ID: " + bottleId);
-        System.out.println("Capacity: " + capacity + " liters");
-        System.out.println("Material: " + material);
-        System.out.println("Is Reusable: " + (isReusable ? "Yes" : "No"));
-        System.out.println("Color: " + color);
-        System.out.println("Is Sealed: " + (isSealed ? "Yes" : "No"));
-        System.out.println("Is Filled: " + (isFilled ? "Yes" : "No"));
-        System.out.println("Contents: " + (contents != null ? contents : "Empty"));
-    }
-
     public void openBottle() {
         isSealed = false;
         System.out.println("Bottle opened.");
